224588e206005756c4b152100159e1be1c58de0a
Michael Chilaka <obichilaka@gmail.com>
2022-01-27 00:15:16 +0100

    added Types.tsx

---
+++ src/Types.tsx
@@ -0,0 +1,20 @@
+let name: string;
+name = "Michael"
+// STRING TYPES 
+
+let age: number;
+// NUMBER TYPES
+
+let isNeighbours: boolean;
+// BOOLEAN TYPES
+
+let cars: string[];
+// ARRAY OF STRING TYPES
+
+
+
+export const Types = () => {
+  return <div>
+      
+  </div>;
+};
