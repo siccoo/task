6a259848b305723c814d1b50aa89e7cb9399a0f4
Michael Chilaka <obichilaka@gmail.com>
2022-01-27 11:12:18 +0100

    updates on Types.tsx

--- src/Types.tsx
+++ src/Types.tsx
@@ -8,9 +8,45 @@ let age: number;
 let isNeighbours: boolean;
 // BOOLEAN TYPES
 
-let cars: string[];
+let cars: string[]; 
 // ARRAY OF STRING TYPES
 
+let ageBracket: number[];
+// ARRAY OF NUMBER TYPES
+
+let role: [number, string];
+// TUPLE TYPES  
+
+// THERE ARE TWO WAYS IN DEFINING AN OBJECT ON TYPESCRIPT
+
+// FIRST WAY
+type Person = {
+    name: string;
+    age?: number;
+};
+
+let person: Person = {
+    name: "Michael"
+};
+
+// SECOND WAY
+let lotsOfPeople: Person[];
+
+function printName(name: string) {
+    console.log(name);   
+}
+
+printName("Michael Chilaka");
+
+// DECLARING A FUNCTION TYPE IS IN TWO WAYS
+// FIRST WAY
+let allNames: Function;
+
+// let allName: (name: string) => void; 
+let allName: (name: string) => never; 
+
+// DIFFERENCE BETWEEN VOID AND NEVER
+// VOID RETURNS UNDEFINED, WHILE NEVER DOESN'T RETURN ANYTHING
 
 
 export const Types = () => {
