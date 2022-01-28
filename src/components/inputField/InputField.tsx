79d8d98eb81cd6f5ff9e8879c05a1f9e7ccd62c5
Michael Chilaka <obichilaka@gmail.com>
2022-01-28 11:34:27 +0100

    updates on InputField.tsx & styles.css

--- src/components/inputField/InputField.tsx
+++ src/components/inputField/InputField.tsx
@@ -4,6 +4,7 @@ const InputField = () => {
   return (
   <form className='input'>
       <input type="input" placeholder='Enter a Task' className="input__box" />
+      <button type="submit" className='input__submit'>Go</button>
   </form>
   );
 };
